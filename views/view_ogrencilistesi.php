2f2bc2f178e491d4ef2f40604a63552156b42dd1
afsatici <afsatici@gmail.com>
2018-02-21 13:00:28 +0300

    son

--- views/view_ogrencilistesi.php
+++ views/view_ogrencilistesi.php
@@ -19,59 +19,92 @@ if ($sonuc = $vt->query($sql)) {
     echo ("<SCRIPT LANGUAGE='JavaScript'> window.alert('Bir hata oluştu: $vt->error ve SQL: $sql !') </SCRIPT>");
 }
 
-/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYIT OLAN ÖĞRENCİLERİ ALALIM */
-$sql = "SELECT * FROM ogrencialinanders WHERE aktifDersKod = $aktifDersKod";
+/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYITLI VE ONAYLI ÖĞRENCİLERİ ALALIM */
+$sql = "SELECT * FROM ogrencialinanders WHERE aktifDersKod = $aktifDersKod AND onay = 1";
 
 if ($sonuc = $vt->query($sql)) {
     while($satir = $sonuc->fetch_assoc()) {
-        $kayitliogrencilistesi[] = $satir;
+        $onayliogrencilistesi[] = $satir;
     }
 } else {
     echo ("<SCRIPT LANGUAGE='JavaScript'> window.alert('Bir hata oluştu: $vt->error ve SQL: $sql !') </SCRIPT>");
 }
+
+/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYITLI AMA ONAYSIZ OLAN ÖĞRENCİLERİ ALALIM */
+$sql = "SELECT * FROM ogrencialinanders WHERE aktifDersKod = $aktifDersKod AND onay = 0";
+echo $sql;
+if ($sonuc = $vt->query($sql)) {
+  var_dump($sonuc);
+    while($satir = $sonuc->fetch_assoc()) {
+        $onaysizogrencilistesi[] = $satir;
+    }
+} else {
+    echo ("<SCRIPT LANGUAGE='JavaScript'> window.alert('Bir hata oluştu: $vt->error ve SQL: $sql !') </SCRIPT>");
+}
+
 include 'ayar/vtkapat.php';
+var_dump($onayliogrencilistesi);
+var_dump($onaysizogrencilistesi);
+/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYIT OLAN VE ONAYLI ÖĞRENCİLERİ LİSTELEYELİM */
 
-/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYIT OLAN ÖĞRENCİLERİ LİSTELEYELİM */
-
-echo "<h2>Dersi Alan Öğrenciler </h2>";
-/* ÖĞRETMENİN ÖĞRENCİ KAYDEDEBİLMESİ İÇİN FORM */
-echo "<form action='kayit.php' method='POST'>\r\n";
-echo "<input type='hidden' name='aktifDersKod' value='$aktifDersKod'>\r\n";
-echo "<p>Onaylamak istediğiniz öğrencileri seçiniz: </p>\r\n ";
-
-
-if (isset ($kayitliogrencilistesi) AND (count($kayitliogrencilistesi) > 0)){
+if (isset ($onayliogrencilistesi) AND (count($onayliogrencilistesi) > 0)){
+    echo "<h2>Dersi Alan Onaylı Öğrenciler </h2>";
     echo "<table border='1'>";
     echo "<tr>";
     echo "<td> <b>Numara</b> </td>";
     echo "<td> <b>Ad</b> </td>";
     echo "<td> <b>Soyad</b> </td>";
-    echo "<td> <b>Onayla</b> </td>";
     echo "</tr>\r\n";
     foreach ($ogrencilistesi as $ogrenci) {
-        foreach ($kayitliogrencilistesi as $kayitliogrenci)
-        if ($kayitliogrenci["ogrenciKod"] == $ogrenci["kod"]) {
+        foreach ($onayliogrencilistesi as $onayliogrenci)
+        if ($onayliogrenci["ogrenciKod"] == $ogrenci["kod"]) {
             echo "<tr>";
             echo "<td>".$ogrenci["numara"]."</td>";
             echo "<td>".$ogrenci["ad"]."</td>";
             echo "<td>".$ogrenci['soyad']."</td>";
-            if ($kayitliogrenci["onay"]) {
-              echo "<td>&nbsp</td>\r\n";
-            } else {
-              echo "<td><input type='checkbox' name='onaylanacakogrenciler[]' value='".$ogrenci["kod"]."'></td>\r\n";
-            }
             echo "</tr>\r\n";
         }
     }
     echo "</table>\r\n";
-    echo "<br /><input type='submit' value='Onayla' name='ogrencionay'>";
-    echo "</form>";
+} else {
+    echo ("Henüz bu derse kayıtlı ve onaylı öğrenci yok! <br />");
+}
+
+/* ŞİMDİ DERSİN SEÇİLEN OTURUMUNA KAYIT OLAN VE ONAYLI ÖĞRENCİLERİ LİSTELEYELİM */
+
+if (isset ($onaysizogrencilistesi) AND (count($onaysizogrencilistesi) > 0)){
+  echo "<h2>Dersi Alan Onaylı Öğrenciler </h2>";
+  /* ÖĞRETMENİN ÖĞRENCİ KAYDEDEBİLMESİ İÇİN FORM                                */
+  echo "<form action='kaydet.php' method='POST'>\r\n";
+  echo "<input type='hidden' name='aktifDersKod' value='$aktifDersKod'>\r\n";
+  echo "<p>Onaylamak istediğiniz öğrencileri seçiniz: </p>\r\n ";
+  echo "<table border='1'>";
+  echo "<tr>";
+  echo "<td> <b>Numara</b> </td>";
+  echo "<td> <b>Ad</b> </td>";
+  echo "<td> <b>Soyad</b> </td>";
+  echo "<td> <b>Onayla</b> </td>";
+  echo "</tr>\r\n";
+  foreach ($ogrencilistesi as $ogrenci) {
+      foreach ($onaysizogrencilistesi as $onaysizogrenci)
+      if ($onaysizogrenci["ogrenciKod"] == $ogrenci["kod"]) {
+          echo "<tr>";
+          echo "<td>".$ogrenci["numara"]."</td>";
+          echo "<td>".$ogrenci["ad"]."</td>";
+          echo "<td>".$ogrenci['soyad']."</td>";
+          echo "<td><input type='checkbox' name='onaylanacakogrenciler[]' value='".$ogrenci["kod"]."'></td>\r\n";
+          echo "</tr>\r\n";
+      }
+  }
+  echo "</table>\r\n";
+  echo "<br /><input type='submit' value='Onayla' name='ogrencionay'>";
+  echo "</form>";
 } else {
     echo ("Henüz bu derse kayıt olmuş öğrenci yok! <br />");
 }
 
+/* ÖĞRETMENİN ÖĞRENCİ KAYDEDEBİLMESİ İÇİN FORM                                */
 echo "<h2> Öğrenci Kayıt </h2>";
-/* ÖĞRETMENİN ÖĞRENCİ KAYDEDEBİLMESİ İÇİN FORM */
 echo "<form action='kayit.php' method='POST'>\r\n";
 echo "<input type='hidden' name='aktifDersKod' value='$aktifDersKod'>\r\n";
 echo "<p>Kayıtlamak istediğiniz öğrencileri seçiniz: </p>\r\n ";
